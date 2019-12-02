2e0f4b9c81d4569b5032825d9b9d8f2794e617da
sagark161 <sagar.k161@gmail.com>
2019-12-02 21:06:51 +0530

    wk49.1:sk:framework

--- Auto_FilpKart/src/test/java/pages/BrowserRepository.java
+++ Auto_FilpKart/src/test/java/pages/BrowserRepository.java
@@ -1,11 +1,32 @@
 package pages;
 
+import java.util.concurrent.TimeUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserRepository {
-	WebDriver driver;
 	
-	public BrowserRepository(WebDriver driver){
-		this.driver = driver;
+	public WebDriver startAppliaction(WebDriver driver, String browserName, String appURL) {
+		switch(browserName){
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			break;
+		case "firefox":
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			break;
+		default:
+			System.out.println("no driver found");			
+		}
+	driver.get(appURL);
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	return driver;
 	}
+
 }
---
+++ Auto_FilpKart/src/test/java/scenarios/ToTest.java
@@ -0,0 +1,10 @@
+package scenarios;
+
+import org.testng.annotations.Test;
+
+public class ToTest {
+	@Test
+	public void test1(){
+		
+	}
+}
