023fd5cbf3a84e88032a5c7c5d89db8f94f721ea
sagark161 <sagar.k161@gmail.com>
2019-12-17 23:08:09 +0530

    wk50:sk:

--- Auto_FilpKart/src/test/java/scenarios/Scenario2.java
+++ Auto_FilpKart/src/test/java/scenarios/Scenario2.java
@@ -1,11 +1,30 @@
 package scenarios;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.*;
 
 public class Scenario2 extends BaseClass{
 	@Test
 	public void testSearch(){
-		//home.searchProduct("shoes");
-		
+		//home.searchProduct("shoes");		
+		try {
+			File f = new File("SearchQueries.xlsx");
+			FileInputStream fis = new FileInputStream(f);
+			Workbook wb = new XSSFWorkbook(fis);
+			Sheet s = wb.getSheet("Sheet1");
+			Row r = s.getRow(3);
+			System.out.println(r.getCell(2).getStringCellValue());
+		} catch (Exception e) {
+			//System.out.println("Failed to read excel");
+			System.out.println(e.getMessage());;			
+		}
 	}
 }
